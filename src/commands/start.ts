79dcc7207b82595b678f51407bf52ac592f8ddfc
renata tsedenova <recedenova@gmail.com>
2025-02-04 01:46:36 +0400

    [B-2] Improvements are needed, the bot works somehow for now (very badly)

--- src/commands/start.ts
+++ src/commands/start.ts
@@ -1,7 +1,18 @@
-import { Context } from 'telegraf';
+import { Context, Markup } from "telegraf";
 
 export const startCommand = (ctx: Context) => {
-  ctx.reply('Привет! Что вы хотите сделать?', {
-    
-  });
+  ctx.reply(
+    "🔹 Кнопка <b>Track NFT</b> — начинает отслеживание NFT.\n" +
+    "🔹 Кнопка <b>Untrack NFT</b> — удаляет NFT из списка отслеживаемых.\n" +
+    "🔹 Кнопка <b>List</b> — показывает список отслеживаемых NFT.",
+    {
+      parse_mode: "HTML", 
+      reply_markup: Markup.inlineKeyboard([
+        [
+          Markup.button.callback("Track NFT", "start_tracking_nft"),
+          Markup.button.callback("Untrack NFT", "untrack_nft")  
+        ]
+      ]).reply_markup 
+    }
+  );
 };
